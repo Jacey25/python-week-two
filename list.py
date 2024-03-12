7cbd61efd3c3890d4800ac664c3dd086307b99d9
Jacey25 <159909692+Jacey25@users.noreply.github.com>
2024-03-12 02:47:03 -0700

    Add files via upload

---
+++ list.py
@@ -0,0 +1,24 @@
+my_list=[]
+# appending value
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# inserting value
+my_list.insert(1,15)
+
+# extendingalist
+list_two=[50,60,70]
+my_list.extend(list_two)
+
+# removinganitem
+my_list.remove(70)
+
+# sortingalist
+my_list.sort()
+
+# findingindexofavalue
+i=my_list.index(30)
+
+print(i)
